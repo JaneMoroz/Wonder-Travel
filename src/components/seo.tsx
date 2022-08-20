de15be7ad987c0423f3c8ca1f4e146d13e948f82
Jane Moroz <Moroz.jane@yahoo.com>
2022-08-20 15:27:00 +0300

    Initialize project using Gatsby Default Starter

---
+++ src/components/seo.tsx
@@ -0,0 +1,61 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+interface SeoProps {
+  description: string
+  title: string
+  children: React.FC
+}
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+function Seo({ description, title, children }: SeoProps) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <>
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={metaDescription} />
+      {children}
+    </>
+  )
+}
+
+Seo.defaultProps = {
+  description: ``,
+}
+
+Seo.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string.isRequired,
+}
+
+export default Seo
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const IndexPage: React.FC = props => {
+  return <Layout>Wonder Travel Home Page!</Layout>
+}
+
+export default IndexPage
